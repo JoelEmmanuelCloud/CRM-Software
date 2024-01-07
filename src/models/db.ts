2211a4046dc47d4899ca9396dcd79e7d0fa8ff48
Joel Emmanuel <ejoel0035@gmail.com>
2024-01-07 14:16:22 +0100

    feat: add db

---
+++ src/models/db.ts
@@ -0,0 +1,50 @@
+import * as mysql from 'mysql';
+
+function getConnection(callback: (connection: mysql.Connection) => void): void {
+  const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'crm'
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
+
+  callback(connection);
+}
+
+export const db = {
+  getResults: (sql: string, callback: (results: any[]) => void): void => {
+    getConnection((connection) => {
+      connection.query(sql, (error, results) => {
+        callback(results);
+      });
+
+      connection.end((err) => {
+        console.log('connection closed!');
+      });
+    });
+  },
+
+  execute: (sql: string, callback: (status: boolean) => void): void => {
+    getConnection((connection) => {
+      connection.query(sql, (error, result) => {
+        if (result) {
+          callback(true);
+        } else {
+          callback(false);
+        }
+      });
+
+      connection.end((err) => {
+        console.log('connection closed!');
+      });
+    });
+  }
+};
